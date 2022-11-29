910aa3c44b5708dfac0498f8d2fc3e4fa91ca257
lufemoal19 <lufemoal19@gmail.com>
2022-11-29 01:42:33 -0600

    testing parsing

--- parsing.pl
+++ parsing.pl
@@ -1,45 +1,49 @@
 /*
-Demo of parsing
-@author loriacarlos@gmail.com
+@about PARSER URQUERY 
+@authors grupo 03 - 1PM
 @since 2022
 */
 
 :- use_module(lexing).
-main_urquery([L | R]) -->  ws, (prog_urquery(L);let(L);tagquery(L);forquery(L);exprquery(L);sourcequery(L);docpath(L);varquery(L);vartag(L);varpath(L);startxpath(L);xpath(L);qvar(L)), ws, main_urquery(R), {!}.
+
+main_urquery([L | R]) -->  ws, (prog_urquery(L);varquery(L);vartag(L);varpath(L)), ws, main_urquery(R), {!}.
 main_urquery([]) --> [].
 
 let(let(X, E)) --> ws, "let", ws, id(X), ws, "=", ws, expr(E), ws.
 
-id(id(I)) --> identifier(I).
-xml_id(xml_id(I)) --> identifier(I).
-num(num(N)) -->  number(N).
-js_string(js_expr(I)) --> "'", identifier(I), "'".
 
-expr(X) --> id(X);num(X);js_string(X).
+
+id(id(I)) --> identifier(I).
+xml_id(I) --> identifier(I).
+num(num(N)) -->  number(N).
+uq_string(expr(I)) --> "'", identifier(I), "'".
+
+expr(X) --> id(X);num(X);uq_string(X).
 
 % qvar: $variable
 qvar(qvar(I)) --> ws, [36], id(I), ws.
-tag(I) --> ws, xml_id(I), ws.
+tag(tag(I)) --> ws, xml_id(I), ws.
 
 xpath(xpath(I, R)) --> tag(I), "/", xpath(R).
 xpath(xpath(I)) --> tag(I). 
-startxpath(startxpath(I)) --> "/", xpath(I).
+startxpath(I) --> "/", xpath(I).
 
 %$galleta/sabor 
 % gramatica varpath varpath ->qvar (startxpath)?;
 varpath(varpath(I, P)) --> qvar(I), startxpath(P).
 
 % vartag -> "<" tag ">" "{" varpath "}" "</" tag ">";
-vartag(vartag(T, I)) --> ws, "<", tag(T), ">", "{",varpath(I), "}", "</", tag(T), ">", ws.  
+vartag(vartag(T,I)) --> ws, "<", tag(T), ">", ws, "{", ws, varpath(I), ws, "}", ws, "</", tag(T), ">", ws. 
 
-% varquery -> vartag | varpath
-varquery(varquery(I)) --> vartag(I); varpath(I).
+% varquery -> vartag | varpath varquery(T, I) --> vartag(T,I).
+varquery(vq(I)) --> vartag(I); varpath(I).
+
 
 % docpath -> "doc" "(" expr ")"
 docpath(docpath(E)) --> ws, "doc", ws, "(", expr(E), ")", ws.
 
 %sourcequery -> docpath | qvar
-sourcequery(sourcequery(I)) --> docpath(I) ; qvar(I). 
+sourcequery(I) --> docpath(I) ; qvar(I). 
 
 %exprquery -> sourcequery ( startxpath )? ;
 exprquery(exprquery(I, P)) --> sourcequery(I), startxpath(P).
@@ -51,11 +55,12 @@ forquery(forquery(V, I, P)) --> ws, "for", ws, qvar(V), ws, "in", ws, exprquery(
 tagquery(tagquery(T,I)) --> ws, "<", tag(T), ">", ws, "{", ws, forquery(I), ws, "}", ws,  "</", tag(T), ">", ws. 
 
 %urquery -> tagquery;
-urquery(uq(I)) --> tagquery(I).
+urquery(I) --> tagquery(I).
 
 letprog(letprog(I, E, U)) --> ws, "let", ws, id(I), ws, "=", ws, expr(E), ws, "in", ws, urquery(U).
 
 prog_urquery(prog(I)) --> (letprog(I); urquery(I)), {!}.
+prog_urquery([]) --> [].
 %%%%%%%%%%%%%%%%%%%%%%%%%% Lexer Xquery Utils %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 test(L) :- 
