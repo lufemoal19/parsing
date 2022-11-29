fc491106d4b42cd05587b2a04c02ad82b27ba0a6
lufemoal19 <lufemoal19@gmail.com>
2022-11-28 23:22:09 -0600

    PARSER FINISHED :D

--- lexing.pl
+++ lexing.pl
@@ -6,10 +6,10 @@ Demo of lexing
 :- module(lexing, [identifier/3, ws/2]).
 :- use_module(library(dcg/basics)).
 :- reexport(library(dcg/basics), [number/3]).
-
-identifier(I) --> blanks, [C],{string(C);letter(C)}, rest_identifier(R), 
+%is_space(C);is_white(C);
+identifier(I) --> blanks, [C],{is_period(C);string(C);letter(C);num(C)}, rest_identifier(R), 
                   blanks, {atom_codes(I, [C|R])}.
-rest_identifier([C|R]) --> [C], {string(C);letter(C);num(C)}, {!}, rest_identifier(R).
+rest_identifier([C|R]) --> [C], {is_period(C);string(C);letter(C);num(C)}, {!}, rest_identifier(R).
 rest_identifier([]) --> [].
 
 xml_identifier(I) --> blanks, [C],{string(C);letter(C)}, rest_xml_identifier(R), 
@@ -24,6 +24,9 @@ rest_expression([]) --> [].
 letter(C) :- code_type(C, alpha).
 num(C) :- code_type(C, digit).
 string(C) :- code_type(C, ascii).
+is_period(C)  :- code_type(C, period).
+is_space(C)   :- code_type(C, space).
+is_white(C)   :- code_type(C, white).
 
 is_id(A) :- atom_codes(A, Codes), phrase(identifier, Codes).
 
--- parsing.pl
+++ parsing.pl
@@ -5,9 +5,9 @@ Demo of parsing
 */
 
 :- use_module(lexing).
-
-urquery([L | R]) -->  ws, (let(L);vartag(L);varpath(L);startxpath(L);xpath(L);qvar(L)), ws, urquery(R), {!}.
-urquery([]) --> [].
+% 
+main_urquery([L | R]) -->  ws, (prog_urquery(L);let(L);tagquery(L);forquery(L);exprquery(L);sourcequery(L);docpath(L);varquery(L);vartag(L);varpath(L);startxpath(L);xpath(L);qvar(L)), ws, main_urquery(R), {!}.
+main_urquery([]) --> [].
 
 let(let(X, E)) --> ws, "let", ws, id(X), ws, "=", ws, expr(E), ws.
 
@@ -19,24 +19,44 @@ js_string(js_expr(I)) --> "'", identifier(I), "'".
 expr(X) --> id(X);num(X);js_string(X).
 
 % qvar: $variable
-qvar(qvar(I)) --> ws, [36], identifier(I), ws.
+qvar(qvar(I)) --> ws, [36], id(I), ws.
 tag(I) --> ws, xml_id(I), ws.
 
-%/catalogo/producto/nombre
-% xpath "/" tag(I)
 xpath(xpath(I, R)) --> tag(I), "/", xpath(R).
 xpath(xpath(I)) --> tag(I). 
-
 startxpath(startxpath(I)) --> "/", xpath(I).
 
 %$galleta/sabor 
 % gramatica varpath varpath ->qvar (startxpath)?;
-%varpath(xml_id(I)) --> qvar(I), "(", startxpath(I), ")", "?".
 varpath(varpath(I, P)) --> qvar(I), startxpath(P).
 
 % vartag -> "<" tag ">" "{" varpath "}" "</" tag ">";
 vartag(vartag(T, I)) --> ws, "<", tag(T), ">", "{",varpath(I), "}", "</", tag(T), ">", ws.  
 
+% varquery -> vartag | varpath
+varquery(varquery(I)) --> vartag(I); varpath(I).
+
+% docpath -> "doc" "(" expr ")"
+docpath(docpath(E)) --> ws, "doc", ws, "(", expr(E), ")", ws.
+
+%sourcequery -> docpath | qvar
+sourcequery(sourcequery(I)) --> docpath(I) ; qvar(I). 
+
+%exprquery -> sourcequery ( startxpath )? ;
+exprquery(exprquery(I, P)) --> sourcequery(I), startxpath(P).
+
+%forquery -> for qvar in exprquery return varquery;
+forquery(forquery(V, I, P)) --> ws, "for", ws, qvar(V), ws, "in", ws, exprquery(I), ws, "return", ws, varquery(P). 
+
+%tagquery < tag > { forquery } </tag>
+tagquery(tagquery(T,I)) --> ws, "<", tag(T), ">", ws, "{", ws, forquery(I), ws, "}", ws,  "</", tag(T), ">", ws. 
+
+%urquery -> tagquery;
+urquery(uq(I)) --> tagquery(I).
+
+letprog(letprog(I, E, U)) --> ws, "let", ws, id(I), ws, "=", ws, expr(E), ws, "in", ws, urquery(U).
+
+prog_urquery(prog(I)) --> letprog(I); urquery(I).
 %%%%%%%%%%%%%%%%%%%%%%%%%% Lexer Xquery Utils %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 test(L) :- 
@@ -44,5 +64,5 @@ test(L) :-
     read_file_to_codes(File, Codes, []),
     atom_codes(Input, Codes),
     format('Input=~n~s~n', [Input]),
-    phrase(urquery(L), Codes)
+    phrase(main_urquery(L), Codes)
 .
