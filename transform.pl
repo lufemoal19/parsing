e42687a74c83aa0d8e00095a8d2817f337c164d0
lufemoal19 <lufemoal19@gmail.com>
2022-12-14 02:07:33 -0600

    update transform.pl

--- transform.pl
+++ transform.pl
@@ -5,9 +5,9 @@
     lufemoal19@gmail.com
 @since 2022
 */
-   %Main = function(id(main)),
+
 :- module(transform, [toJS/2]).
-%[ur_doc, ur_evaluate, ur_tag, ur_active_doc]
+
 toJS(sequence(L), sequence(JS)) :-
    Import = import(args([ur_doc, ur_evaluate, ur_tag, ur_active_doc]),library('urquery.mjs')),
    Comment = comment('Automatically converted'),
